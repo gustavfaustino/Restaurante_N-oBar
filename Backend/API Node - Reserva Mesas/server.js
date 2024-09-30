223bc1f98957b7fb5584a58533c9e3cd05fe9447
gustavfaustino <gustav.faustino107@gmail.com>
2024-09-30 19:02:11 -0300

    Adição de tratamento de erro sobre o servidor e rota não encontrada

--- Backend/API Node - Reserva Mesas/server.js
+++ Backend/API Node - Reserva Mesas/server.js
@@ -8,6 +8,20 @@ app.get('/', (req, res) => {
     res.send('Bem-vindo ao NoBar API!') // Rota raiz
 })
 
+// Rotas de autenticação
 app.use('/api', publicRoutes) // Adiciona as rotas públicas
 
+// Middleware para tratar erros
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).json({ error: 'Ocorreu um erro no servidor' })
+})
+
+// Middleware para tratar rotas inexistentes
+app.use((req, res, next) => {
+    console.error('Rota não encontrada')
+    res.status(404).json({ error: 'Rota não encontrada' })
+})
+
+// Inicia o servidor
 app.listen(3000, ()=> console.log("Servidor Rodando na porta 3000")) // Inicia o servidor na porta 3000
